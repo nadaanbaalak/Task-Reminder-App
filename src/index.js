2cc9349bd934b0fac7612bb27637689b5d09093c
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-02-21 11:43:36 +0530

    Trying Freecodecamp fix

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ const app = express();
 connectDB();
 
 
+
+//Set static folder
+app.use('/',express.static(path.join(__dirname,'../client/build')));
+    
+
+
 //to parse the incoming Request Data
 app.use(express.json({extended:false}));
 app.use('/api/users',userRouter);
@@ -22,14 +28,11 @@ app.use('/api/auth',authRouter);
 app.use('/api/profile',profileRouter);
 
 //Serve static assets in production
-if(process.env.NODE_ENV==='production') {
-    //Set static folder
-    app.use(express.static(`../client/build`));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, '..','client', 'build','index.html'))
-    });
 
-} 
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../client/build','index.html'))
+});
+
 
 const port = process.env.PORT || 5000;
 checkOverdue();
