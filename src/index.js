74baae2dea10c633cdc8cceedc35cc1a97d69583
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-02-21 10:07:55 +0530

    Made necessary file path changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ connectDB();
 
 
 //to parse the incoming Request Data
-app.use(express.json());
+app.use(express.json({extended:false}));
 app.use('/api/users',userRouter);
 app.use('/api/tasks',taskRouter);
 app.use('/api/auth',authRouter);
@@ -24,9 +24,9 @@ app.use('/api/profile',profileRouter);
 //Serve static assets in production
 if(process.env.NODE_ENV==='production') {
     //Set static folder
-    app.use(express.static(`${__dirname}/client/build`));
+    app.use(express.static(`../client/build`));
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+        res.sendFile(path.resolve(__dirname, '..','client', 'build','index.html'))
     });
 
 } 
