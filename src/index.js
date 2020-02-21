ad5ba7004593605eac2a58d0d43dd8292d45df55
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-02-21 11:57:12 +0530

    Reverted to Previous postbuild script

--- src/index.js
+++ src/index.js
@@ -14,11 +14,14 @@ const app = express();
 connectDB();
 
 
+if(process.env.NODE_ENV==='production') {
+    //Set static folder
+    app.use(express.static('../client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, '..','client', 'build','index.html'))
+    });
 
-//Set static folder
-app.use('/',express.static(path.join(__dirname,'../client/build')));
-    
-
+} 
 
 //to parse the incoming Request Data
 app.use(express.json({extended:false}));
@@ -28,10 +31,6 @@ app.use('/api/auth',authRouter);
 app.use('/api/profile',profileRouter);
 
 //Serve static assets in production
-
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, '../client/build','index.html'))
-});
 
 
 const port = process.env.PORT || 5000;
