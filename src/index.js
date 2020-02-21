1a5256e32b49173ea0d5fe1f5fcea4ecf5d52cc7
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-02-21 12:12:55 +0530

    Reverted to Previous postbuild script

--- src/index.js
+++ src/index.js
@@ -16,10 +16,7 @@ connectDB();
 
 if(process.env.NODE_ENV==='production') {
     //Set static folder
-    app.use(express.static('../client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, '..','client', 'build','index.html'))
-    });
+    app.use(express.static(path.resolve(__dirname, '..','client', 'build')));
 
 } 
 
@@ -31,7 +28,9 @@ app.use('/api/auth',authRouter);
 app.use('/api/profile',profileRouter);
 
 //Serve static assets in production
-
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, '..','client', 'build','index.html'))
+});
 
 const port = process.env.PORT || 5000;
 checkOverdue();
